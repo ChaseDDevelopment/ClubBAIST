7f57273acce4ef7d375cb4c26c833c3163cd7e65
Chase Dubauskas <Chase.D.Dubauskas@gmail.com>
2019-11-24 00:02:31 -0700

    Changed SQL Script

--- ClUBBAIST.sql
+++ ClUBBAIST.sql
@@ -11,6 +11,8 @@ IF NOT EXISTS (
 CREATE DATABASE CLUBBAIST
 GO
 
+USE CLUBBAIST
+GO
 
 -- Create a new table called 'TeeTime' in schema 'SchemaName'
 -- Drop the table if it already exists
@@ -21,7 +23,7 @@ GO
 CREATE TABLE TeeTime
 (
     [Date] DATETIME NOT NULL, -- primary key column
-    [Time] DATETIME NOT NULL,
+    [Time] TIME NOT NULL,
     Golfer1 [NVARCHAR](25) NOT NULL,
     Golfer2 [NVARCHAR](25),
     Golfer3 [NVARCHAR](25),
@@ -70,3 +72,120 @@ CREATE TABLE StandingTeeTime
     -- specify more columns here
 );
 GO
+
+IF EXISTS (
+SELECT *
+    FROM INFORMATION_SCHEMA.ROUTINES
+WHERE SPECIFIC_NAME = N'FindDailyTeeSheet'
+)
+DROP PROCEDURE FindDailyTeeSheet
+GO
+-- Create the stored procedure
+CREATE PROCEDURE FindDailyTeeSheet
+(
+    @Date DATETIME = NULL
+)
+AS
+    DECLARE @ReturnCode INT
+    SET @ReturnCode = 1
+    
+    IF @Date IS NULL
+        RAISERROR('FindDailyTeeSheet Failed - Required Parameter: @Date',16,1)
+    ELSE
+        BEGIN
+
+        SELECT * FROM TeeTime
+        WHERE [Date] = @Date
+
+        IF @@ERROR = 0
+            SET @ReturnCode = 0
+        ELSE RAISERROR('FindDailyTeeSheet Failed - Select Error in Database',16,1)
+        END
+        RETURN @ReturnCode
+GO
+
+IF EXISTS(
+SELECT *
+    FROM INFORMATION_SCHEMA.ROUTINES
+WHERE SPECIFIC_NAME = N'CreateTeeTime'
+)
+DROP PROCEDURE CreateTeeTime
+GO
+
+CREATE PROCEDURE CreateTeeTime
+(
+    @Date DATETIME = NULL,
+    @Time TIME = NULL,
+    @Golfer1 NVARCHAR(25) = NULL,
+    @Golfer2 NVARCHAR(25) = NULL,
+    @Golfer3 NVARCHAR(25) = NULL,
+    @Golfer4 NVARCHAR(25) = NULL
+)
+AS 
+    DECLARE @ReturnCode INT
+    SET @ReturnCode = 1
+    IF @Date IS NULL AND @Time IS NULL AND @Golfer1 IS NULL
+        RAISERROR('CreateTeeTime Failed - Required Parameters: All',16,1)
+    ELSE
+    IF @Date IS NULL
+        RAISERROR('CreateTeeTime Failed - Required Parameters: Date', 16,1)
+    ELSE
+    IF @Time IS NULL
+        RAISERROR('CreateTeeTime Failed - Required Parameters: Time',16,1)
+    ELSE
+    IF @Golfer1 IS NULL
+        RAISERROR('CreateTeeTime Failed- Required Parameters: Time', 16,1)
+    ELSE
+        BEGIN
+        INSERT INTO TeeTime([Date],[Time],Golfer1,Golfer2,Golfer3,Golfer4)
+        VALUES (@Date, @Time, @Golfer1, @Golfer2, @Golfer3, @Golfer4)
+
+        IF @@ERROR = 0
+            SET @ReturnCode = 0
+        ELSE RAISERROR('CreateTeeTime Failed - Insert Error in Database',16,1)
+        END
+        RETURN @ReturnCode
+GO
+
+
+-- Insert rows into table 'TeeTime'
+INSERT INTO TeeTime
+( -- columns to insert data into
+ [Date], [Time], Golfer1, Golfer2, Golfer3, Golfer4
+)
+VALUES
+( -- first row: values for the columns in the list above
+ '2020-08-15', '07:00:00', 'Chase', 'Haley', 'Rob', 'Stark'
+),
+(
+'2020-08-15', '07:07:00', 'Marty', 'Pam', 'Braeden', 'Phil'
+),
+(
+'2020-08-15', '07:30:00', 'Hello', 'GolfLover', null, null
+),
+(
+'2020-08-15', '08:15:00', 'Marty', 'Pam', 'Braeden', 'Phil'
+),
+(
+'2020-08-15', '09:07:00', 'Marty', 'Pam', 'Braeden', 'Phil'
+),
+(
+'2020-08-15', '10:07:00', 'Marty', 'Pam', 'Braeden', 'Phil'
+),
+(
+'2020-08-15', '17:07:00', 'Marty', 'Pam', 'Braeden', 'Phil'
+),
+(
+'2020-08-15', '15:07:00', 'Marty', 'Pam', 'Braeden', 'Phil'
+),
+(
+'2020-08-15', '16:07:00', 'Marty', 'Pam', 'Braeden', 'Phil'
+),
+(
+'2020-08-15', '18:07:00', 'Marty', 'Pam', 'Braeden', 'Phil'
+)
+
+-- add more rows here
+GO
+
+Execute FindDailyTeeSheet '2020-08-15'
